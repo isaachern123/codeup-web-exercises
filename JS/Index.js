a778dd7f6c18b9095c26b4313b581c3cc3a89e7c
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2022-11-22 11:23:04 -0600

    javascript in HTML

--- JS/Index.js
+++ JS/Index.js
@@ -1,4 +1,21 @@
 "use strict"
-let age = prompt("How old are you??")
 
-alert(`You are ${age} years old`);
+// console.log("Hello from External JavaScript")
+//
+//
+// alert("Welcome to my website!");
+//  let color = prompt('What is your favorite color?')
+// alert(`Great, ${color} is my favorite color too!`)
+
+
+let mermaid = prompt('How many days did you rent Little Mermaid for?')
+
+let bear = prompt('How many days did you rent Brother Bear for?')
+
+let hercules = prompt('How many days did you rent Hercules for?')
+
+let Total = (parseInt(bear) + parseInt(hercules) + parseInt(mermaid)) * 3
+
+alert(`Your total is $ ${Total}`)
+
+
