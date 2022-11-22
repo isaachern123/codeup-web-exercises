f5bc6986a4bb5232c0191205a4355f11940ebdd1
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2022-11-22 15:01:36 -0600

    Functions

--- JS/Index.js
+++ JS/Index.js
@@ -8,14 +8,28 @@
 // alert(`Great, ${color} is my favorite color too!`)
 
 
-let mermaid = prompt('How many days did you rent Little Mermaid for?')
+// let mermaid = prompt('How many days did you rent Little Mermaid for?')
+//
+// let bear = prompt('How many days did you rent Brother Bear for?')
+//
+// let hercules = prompt('How many days did you rent Hercules for?')
+//
+// let Total = (parseInt(bear) + parseInt(hercules) + parseInt(mermaid)) * 3
+//
+// alert(`Your total is $ ${Total}`)
 
-let bear = prompt('How many days did you rent Brother Bear for?')
+//
+// function LifeInWeeks() {
+//     let age = prompt('How old are you?')
+//     let AgeinWeeks = (age * 52)
+//     alert(`You have been alive for ${AgeinWeeks} weeks!`)
+//
+// }
+//
+// LifeInWeeks()
 
-let hercules = prompt('How many days did you rent Hercules for?')
 
-let Total = (parseInt(bear) + parseInt(hercules) + parseInt(mermaid)) * 3
 
-alert(`Your total is $ ${Total}`)
+
 
 
