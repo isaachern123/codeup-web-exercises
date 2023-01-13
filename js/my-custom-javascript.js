2e2465dcd92b54dc3a626091f59b0f73cb66c9f5
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-01-13 13:34:08 -0600

    added J-Query

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(function() {
+
+    $('.codeup').css('border', '1px solid red')
+    $('li, h1, p').css('font-size', '20px') .css('background-color', '#FFFF00')
+    let contents = $('h1').html();
+    alert(contents);
+
+});
+
