70ddd3d957ec2c7f23aebbea3f086e5b6fecfca3
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-01-26 11:57:18 -0600

    stuck on last 2 reduces

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,63 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+let threeLang = users.filter(function (user) {
+    return user.languages.length > 2;
+})
+console.log(threeLang)
+
+let emailArray = users.map(function(user) {
+    return user.email
+})
+console.log(emailArray)
+
+const averageExperience = users.reduce((total, user) => {
+    return total + user.yearsOfExperience / 5;
+}, 0);
+
+console.log(averageExperience)
+
+// const longestEmail = users.reduce((longestEmailSoFar, email) => {
+//     if()
+// }, 0);
+
+const userNames = users.reduce(function (allNames, user) {
+
+})
+
+console.log(userNames)
